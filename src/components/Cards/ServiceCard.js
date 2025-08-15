bdcb880dad8d0eac50b4af246d34125c35357af8
Alok Singh <thealoksinghh@gmail.com>
2025-08-15 23:48:23 +0530

    carousel added

--- src/components/Cards/ServiceCard.js
+++ src/components/Cards/ServiceCard.js
@@ -1,13 +1,21 @@
 import React from 'react';
-
+import { motion } from "framer-motion";
 function ServiceCard() {
   return (
    
-    <div
-      className="group w-full md:w-[22rem] lg:w-[20rem] relative h-[28rem] [perspective:1000px] "
+    <motion.div
+    initial={{ opacity: 0, y: 50 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      transition={{
+        duration: 0.6,
+        ease: "easeOut",
+        delay: 2 * 0.2, // Adjust delay based on the index of the card instead of hardcoding 2 put index here
+      }}
+      viewport={{ once: true }}
+      className="group  mt-16 w-full md:w-[20rem] lg:w-[20rem] relative h-[28rem] [perspective:1000px] "
     >
        <div
-        className="absolute w-full h-full flex flex-col items-center justify-center px-6 transition-transform duration-700 [backface-visibility:hidden] [transform:rotateY(0deg)] group-hover:[transform:rotateY(180deg)] rounded-xl border border-white/10 backdrop-blur-xl shadow-[var(--shadow-glow)] bg-[var(--card-background)] "
+        className="absolute w-full h-full flex flex-col items-center justify-center px-4 transition-transform duration-700 [backface-visibility:hidden] [transform:rotateY(0deg)] group-hover:[transform:rotateY(180deg)] rounded-xl border border-white/10 backdrop-blur-xl shadow-[var(--shadow-glow)] bg-[var(--card-background)] "
       >
          <div className="absolute -top-16 left-1/2 -translate-x-1/2 border-2 border-white rounded-full overflow-hidden">
           <div className="w-32 h-32 bg-[var(--brand-secondary)] rounded-full shadow-lg overflow-hidden ring-4 ring-[var(--card-background)]">
@@ -20,7 +28,7 @@ function ServiceCard() {
         </div>
 
          <div className="mt-16 text-center">
-          <h2 className="text-2xl font-bold text-white">Service One</h2>
+          <h2 className="text-2xl font-bold text-white">title here</h2>
           <p className="mt-3 text-sm text-slate-300 leading-relaxed">
             lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
             <span className="text-[var(--brand-accent)] font-medium">
@@ -46,7 +54,7 @@ function ServiceCard() {
             Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
           </span>.
         </p>
-      </div></div>
+      </div></motion.div>
     
    
   );
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -3,16 +3,17 @@ import DemoCard from "../components/DemoCard";
 import ServiceCard from "../components/Cards/ServiceCard";
 import ClientCard from "../components/Cards/ClientCard";
 import ProjectImages from "../components/Cards/ProjectImages";
+import BouncyText from "../components/BouncyText";
 
 function Home() {
   return (
-    <div className="w-full pt-24 px-4 py-10 md:py-14 bg-[var(--brand-primary)] ">
+    <div className="w-full pt-24 py-10 md:py-14 bg-[var(--brand-primary)] ">
       <div className="flex justify-center">
         <div className="flex flex-col lg:flex-row items-center gap-8">
           <div className="relative w-[30%] h-[40%] rounded-lg  ring-1 ring-white/10"></div>
 
           <div className="flex-1 text-center lg:text-left">
-            <h1 className="text-[3rem] md:text-[4rem] xl:text-[5rem] font-extrabold tracking-tight text-white">
+            <h1 className="text-[2rem] md:text-[3rem] xl:text-[4rem] font-extrabold tracking-tight text-white">
               Innovative technology solutions
             </h1>
             <p className="mt-2 text-[#9aa4b2] text-base md:text-lg">
@@ -39,16 +40,38 @@ function Home() {
           </div>
         </div>
       </div>
-      <div className="p-20 md:mt-16 flex flex-wrap justify-center gap-6">
-        <ServiceCard />
-        <ServiceCard />
-        <ServiceCard />
-      </div>
+
       <div className="p-10 md:mt-16 flex flex-wrap justify-center gap-6">
-        <ClientCard />
-        <ClientCard />
-        <ClientCard />
+        <ServiceCard />
+        <ServiceCard />
+        <ServiceCard />
       </div>
+
+      <div className="overflow-hidden relative w-full py-16">
+        <div className="flex gap-6 animate-scroll">
+          <div className="flex-none">
+            <ClientCard />
+          </div>
+          <div className="flex-none">
+            <ClientCard />
+          </div>
+          <div className="flex-none">
+            <ClientCard />
+          </div>
+          <div className="flex-none">
+            <ClientCard />
+          </div>
+          <div className="flex-none">
+            <ClientCard />
+          </div>
+        </div>
+      </div>
+
+      <BouncyText
+        text={`A Quick Glimpse of Our Work`}
+        className="text-2xl sm:text-4xl md:text-5xl text-white  px-4 mt-3 font-extrabold leading-tight text-center  "
+      />
+
       <div className="p-10 md:mt-16 flex flex-col lg:flex-row justify-center gap-6">
         <ProjectImages />
         <div className="flex justify-center">
@@ -95,6 +118,53 @@ function Home() {
             </div>
           </div>
         </div>
+      </div>
+      <div className="p-10 md:mt-16 flex flex-col lg:flex-row justify-center gap-6">
+        <div className="flex justify-center">
+          <div className="flex flex-col lg:flex-row items-center gap-8">
+            <div className="flex-1 text-center lg:text-left">
+              <h1 className="text-[2rem] md:text-[1rem] xl:text-[2rem] font-bold tracking-tight text-white">
+                Title of the Project
+              </h1>
+              <p className="mt-2 text-[#9aa4b2] text-base text-sm">
+                Date: 2024-25
+              </p>
+
+              <p className="mt-5 text-sm md:text-base text-slate-300/90 max-w-2xl">
+                Empowering industries to operate smarter, safer, and more
+                efficiently.{" "}
+                <span className="text-white font-semibold">
+                  {" "}
+                  Combining technology, creativity,
+                </span>{" "}
+                and
+                <span className="text-white font-semibold"> expertise,</span>,
+                we drive the future of industrial digitalization.
+              </p>
+              <a
+                href="#"
+                className="group mt-4 inline-flex items-center gap-2 rounded-xl px-5 py-2 bg-[#ef0163] text-white font-semibold hover:translate-y-[-1px] hover:shadow-[0_10px_30px_rgba(239,1,99,0.35)] transition"
+              >
+                View Project
+                <svg
+                  className="w-4 h-4 text-current transition-transform group-hover:scale-110"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M13.828 10.172a4 4 0 010 5.656l-1.414 1.414a4 4 0 01-5.656-5.656l1.414-1.414a4 4 0 015.656 0zM10.172 13.828a4 4 0 010-5.656l1.414-1.414a4 4 0 015.656 5.656l-1.414 1.414a4 4 0 01-5.656 0z"
+                  />
+                </svg>
+              </a>
+            </div>
+          </div>
+        </div>
+        <ProjectImages />
       </div>
     </div>
   );
