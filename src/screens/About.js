6c191ff2db62e354b6bc864d2c6304adc8c45854
Alok Singh <thealoksinghh@gmail.com>
2025-08-26 04:39:21 +0530

    color modified

--- src/screens/About.js
+++ src/screens/About.js
@@ -3,21 +3,10 @@ import HexagonGridBgAbout from "../components/backgrounds/HexagonGridBgAbout";
 
 function About() {
   return (
-    <div>
-      <section className="relative py-16 bg-[var(--brand-primary)] overflow-hidden">
-        <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 z-10">
-          <div className="text-center mb-12">
-            <h2 className="text-3xl md:text-4xl font-extrabold text-[var(--brand-secondary)]">
-              Case Studies
-            </h2>
-            <p className="mt-3 text-lg text-[var(--neutral-gray)]">
-              See how we’ve helped companies transform operations with AI,
-              vision, and smart systems.
-            </p>
-          </div>
-
-        </div>
-      </section>
+    
+      <div className="relative py-16 bg-[var(--brand-primary)] overflow-hidden">
+       
+      
       <HexagonGridBgAbout />
     </div>
   );
