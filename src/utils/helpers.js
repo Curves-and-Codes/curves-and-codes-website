11b225fd8cf8a2577f04160be608068e7fa22d63
Jaysingh Patel <jay229413@gmail.com>
2026-01-16 02:54:10 +0530

    add utility helper functions for date formatting and debouncing

---
+++ src/utils/helpers.js
@@ -0,0 +1,16 @@
+// Utility helper functions
+export const formatDate = (date) => {
+  return new Date(date).toLocaleDateString();
+};
+
+export const debounce = (func, wait) => {
+  let timeout;
+  return function executedFunction(...args) {
+    const later = () => {
+      clearTimeout(timeout);
+      func(...args);
+    };
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+  };
+};
